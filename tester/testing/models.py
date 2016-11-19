188029466d73a0ff2aff2aa7e3ba69e2dff1c07a
Chernov Vladimir <volodya.black@gmail.com>
2016-11-19 14:29:03 +0200

    Модели тестов

--- tester/testing/models.py
+++ tester/testing/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+
+class Test(models.Model):
+    title = models.CharField('заголовок теста', max_length=100, unique=True)
+    creation_date = models.DateField('дата создания', auto_now=True)
+
+    class Meta:
+        verbose_name = 'тест'
+        verbose_name_plural = 'тесты'
+        ordering = ['title']
+
+
+class Question(models.Model):
+    test = models.ForeignKey(Test)
+    text = models.TextField('текст вопроса', max_length=1000)
+
+    class Meta:
+        verbose_name = 'вопрос'
+        verbose_name_plural = 'вопросы'
+
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question)
+
+    class Meta:
+        abstract = True
+
+
+class SimpleTestAnswer(Answer):
+    text = models.TextField('текст ответа', max_length=200)
+    correct = models.BooleanField('правильный?', default=False)
+
+    class Meta:
+        verbose_name = 'ответ (несколько вариантов)'
+        verbose_name_plural = 'ответы (несколько вариантов)'
